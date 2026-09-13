78b6b5e242964ef35b946772ec46dbcacf87d557
Tanishsharma3055 <tanishsharma3055@gmail.com>
2026-09-13 14:49:18 +0530

    Create squareroot.c

---
+++ squareroot.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <math.h>
+ int main (){
+    float a;
+     printf("Enter number whose square root you wnat:\n");
+     scanf("%f",&a);
+     float b;
+     b=sqrt(a);
+     
+     printf("ans is %f",b);
+     return 0;
+ }
